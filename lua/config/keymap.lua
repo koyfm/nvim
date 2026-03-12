2e57033e8fa7d285fd27ac4fcd984ffcfb95aebd
Itzik Koyfman <itzik@koyfman.net>
2026-03-12 10:34:12 +0200

    system clipboard keymaps

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,1 +1,6 @@
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set({ "n" }, "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Put from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', { desc = "Put before from system clipboard" })
+
 vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
